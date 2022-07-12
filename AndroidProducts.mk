4f71aea1289585f71dd519ae3d37bb880d5ccf47
xddeath69 <85794863+xddeath69@users.noreply.github.com>
2022-07-12 19:17:25 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# LineageOS (crDroid)
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/havoc_santoni.mk
+
+COMMON_LUNCH_CHOICES += \
+    havoc_santoni-userdebug \
+    havoc_santoni-user \
+    havoc_santoni-eng
